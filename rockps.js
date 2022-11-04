0e4adfeb5d2302dbdf2be10db3794a9308b33c12
Bradley Griffin <bigriff11@gmail.com>
2022-11-04 08:03:43 -0600

    Add game function to play the game 5 times, keep score, and declare a winner

--- rockps.js
+++ rockps.js
@@ -15,12 +15,12 @@ function getComputerChoice() {
 
 // Play a single round of rpm
 // take two variables, player choice and computer choice
-let playerInput = "ROCK"
+let playerInput = "Rock"
 let computerChoice = "Rock"
 // change player choice to always be all proper noun case
 function capitalize(string) {
     // lower case everything
-    let lower = string.toLowerCase()
+    let lower = string.toString().toLowerCase()
     // capitalize the first letter
     let upper = lower[0].toUpperCase()
     return lower.replace(lower[0], upper)
@@ -34,26 +34,49 @@ function playRound(playerChoice, computerChoice) {
     }
     // if player is rock and computer is paper, computer wins
     if (playerChoice === "Rock" && computerChoice === "Paper") {
+        computerScore += 1
         return "You lose! Paper beats Rock"
     }
     // if player is rock and computer is scissors, player wins
     if (playerChoice === "Rock" && computerChoice === "Scissors") {
+        playerScore += 1
         return "You Win! Rock beats Scissors"
     }
     // if player is paper and computer is scissors, comsputer wins
     if (playerChoice === "Paper" && computerChoice === "Scissors") {
+        computerScore += 1
         return "You lose! Scissors beats Paper"
     }
     // if player is paper and computer is rock, player wins
     if (playerChoice === "Paper" && computerChoice === "Rock") {
+        playerScore += 1
         return "You Win! Paper beats Rock"
     }
     // if player is scissors and computer is paper, player wins
     if (playerChoice === "Scissors" && computerChoice === " Paper") {
+        playerScore += 1
         return "You Win! Scissors beats Paper"
     }
     // if player is scissors and computer is rock, computer wins
     if (playerChoice === "Scissors" && computerChoice === "Rock") {
+        computerScore += 1
         return "You lose! Rock beats Scissors"
     }
 }
+// create a game function where you can play 5 times, it keeps score, and it declares a winners
+let playerScore = 0
+let computerScore = 0
+function game() {
+    // for loop 5 times
+    for(let i = 0; i < 5; i++){
+        console.log(playRound('Rock', getComputerChoice()))
+    }
+    // see who has more points and declare a winner
+    if (playerScore > computerScore) {
+        console.log("Congratulations, you beat the computer!")
+    }
+    else {
+        console.log("Looks like you lost to the computer, better luck next time.")
+    }
+}
+game()
